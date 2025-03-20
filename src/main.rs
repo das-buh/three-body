4196f1c5859d30a2443ee456c7cb991b07c4955d
das-buh <stinkeyecookie@gmail.com>
2025-03-19 20:17:04 -0700

    create render.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use render::*;
+use sim::System;
+use sycamore::prelude::*;
+
+mod render;
 mod sim;
 mod vec;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    sycamore::render(|| "Hello, world!".into());
+    sycamore::render(App);
 }
+
+#[component]
+fn App() -> View {
+    let sim = Sim(create_signal(System::default()));
+    provide_context(sim);
+
+    view! {
+        Bodies()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Sim(Signal<System>);
---
+++ src/render.rs
@@ -0,0 +1,23 @@
+use crate::{vec::Vec2, Sim};
+use sycamore::prelude::*;
+
+#[component]
+pub fn Bodies() -> View {
+    let sim = use_context::<Sim>();
+
+    let bodies = sim.0.map(|system| {
+        system
+            .bodies()
+            .map(|(i, _, p, _)| (i, p))
+            .collect::<Vec<_>>()
+    });
+
+    view! {
+        Indexed(
+            list=bodies,
+            view=|(i, Vec2(x, y))| view! {
+                p { (format!("(i={i} p={x},{y})")) }
+            },
+        )
+    }
+}
