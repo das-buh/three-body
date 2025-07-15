af8e39eb071b3c8e00a2f516082c42a975a1bade
das-buh <stinkeyecookie@gmail.com>
2025-07-14 17:32:08 -0700

    create Settings with run, tickrate

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,39 @@ fn App() -> View {
     let sim = Sim(create_signal(System::default()));
     provide_context(sim);
 
+    provide_context(Settings {
+        run: create_signal(false),
+        tickrate: create_signal(1.),
+    });
+
     view! {
         RenderSim()
         Measurements()
+        Menu()
     }
 }
 
 #[derive(Clone, Copy)]
 struct Sim(Signal<System>);
 
+#[derive(Clone, Copy, Debug)]
+struct Settings {
+    run: Signal<bool>,
+    tickrate: Signal<f64>,
+}
+
 #[component]
 fn Measurements() -> View {
     let sim = use_context::<Sim>();
+    let settings = use_context::<Settings>();
 
     let cm = sim.0.map(|s| s.center_mass());
     let energy = sim.0.map(|s| s.energy());
     let momentum = sim.0.map(|s| s.momentum());
+
+    create_effect(move || {
+        log::debug!("{settings:?}");
+    });
 
     view! {
         div {
@@ -40,5 +57,11 @@ fn Measurements() -> View {
             p { "K + U = " (energy) }
             p { "p = " (momentum.get().to_string()) }
         }
+
+        label(r#for="settings-run") { "start/stop" }
+        input(id="settings-run", r#type="checkbox", bind:checked=settings.run)
+
+        label(r#for="settings-tickrate") { "tickrate" }
+        input(id="settings-tickrate", r#type="range", bind:valueAsNumber=settings.tickrate)
     }
 }
